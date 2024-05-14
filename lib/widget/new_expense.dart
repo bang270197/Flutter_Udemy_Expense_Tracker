a9d668f98dc631cab07321e655ea755c60672334
bangnn.os <bangnn.os@mbbank.com>
2024-05-14 09:01:53 +0700

    textEditController

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -10,15 +10,41 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseSate extends State<NewExpense> {
+  final titleController = TextEditingController();
+
+  // String enteredTile = '';
+
+  // void _saveTitleInput(String value) {
+  //   enteredTile = value;
+  // }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: titleController,
+            // onChanged: _saveTitleInput,
             maxLength: 50,
-            decoration: InputDecoration(label: Text('Tile')),
+            decoration: const InputDecoration(label: Text('Tile')),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(titleController.text);
+                  },
+                  child: const Text('Save Expense'))
+            ],
           )
         ],
       ),
